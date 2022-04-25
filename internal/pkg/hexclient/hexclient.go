d27fcd1511944b07483f71477b784c3d6760f4bd
3vilM33pl3 <ovanac01@mail.bbk.ac.uk>
2022-04-25 18:28:52 +0100

    Squashed commit of the following:
    commit 762c0f1c490c72b2ad693d820318bd7d88f62305
    Author: 3vilM33pl3 <ovanac01@mail.bbk.ac.uk>
    Date:   Mon Apr 25 18:25:34 2022 +0100
    
        split hexrepo/hexmap
    
    commit 5d1ace9b7f6a019f8d2ae2db4dd4c2c9aae46024
    Author: 3vilM33pl3 <ovanac01@mail.bbk.ac.uk>
    Date:   Sun Apr 24 12:43:45 2022 +0100
    
        Implementing new proto (split hex location and info)

--- internal/pkg/hexclient/hexclient.go
+++ internal/pkg/hexclient/hexclient.go
@@ -36,11 +36,11 @@ func NewClient(serverAddr string, secure bool) (c *Client, err error) {
 	return
 }
 
-func (c *Client) RepoAddHexagon(hexList *hexcli.HexRefList) (err error) {
+func (c *Client) RepoAddHexagonInfo(hexInfoList *hexcli.HexInfoList) (err error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	result, err := c.grpcClient.RepoAddHexagons(ctx, hexList)
+	result, err := c.grpcClient.RepoAddHexagonInfo(ctx, hexInfoList)
 
 	if err != nil {
 		return err
@@ -53,11 +53,11 @@ func (c *Client) RepoAddHexagon(hexList *hexcli.HexRefList) (err error) {
 	return nil
 }
 
-func (c *Client) RepoDeleteHexagon(hexList *hexcli.HexRefList) (err error) {
+func (c *Client) RepoDeleteHexagonInfo(hexIDList *hexcli.HexIDList) (err error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	result, err := c.grpcClient.RepoDelHexagons(ctx, hexList)
+	result, err := c.grpcClient.RepoDelHexagonInfo(ctx, hexIDList)
 
 	if err != nil {
 		return err
@@ -70,11 +70,37 @@ func (c *Client) RepoDeleteHexagon(hexList *hexcli.HexRefList) (err error) {
 	return nil
 }
 
-func (c *Client) MapAdd(hex *hexcli.Hex) (err error) {
+func (c *Client) RepoGetHexagonInfo(hexIDList *hexcli.HexIDList) (hexInfoList *hexcli.HexInfoList, err error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	result, err := c.grpcClient.MapAdd(ctx, hex)
+	result, err := c.grpcClient.RepoGetHexagonInfo(ctx, hexIDList)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
+
+func (c *Client) RepoGetAllHexagonInfo() (hexInfoList *hexcli.HexInfoList, err error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	result, err := c.grpcClient.RepoGetAllHexagonInfo(ctx, &hexcli.Empty{})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
+
+func (c *Client) MapAdd(hexLocation *hexcli.HexLocation) (err error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	result, err := c.grpcClient.MapAdd(ctx, hexLocation)
 
 	if err != nil {
 		return err
@@ -87,11 +113,11 @@ func (c *Client) MapAdd(hex *hexcli.Hex) (err error) {
 	return nil
 }
 
-func (c *Client) MapRemove(hexList *hexcli.HexList) (err error) {
+func (c *Client) MapRemove(hexLocationList *hexcli.HexLocationList) (err error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	result, err := c.grpcClient.MapRemove(ctx, hexList)
+	result, err := c.grpcClient.MapRemove(ctx, hexLocationList)
 
 	if err != nil {
 		return err
@@ -104,15 +130,15 @@ func (c *Client) MapRemove(hexList *hexcli.HexList) (err error) {
 	return
 }
 
-func (c *Client) MapGet(hex *hexcli.Hex, radius int64) (hexList *hexcli.HexList, err error) {
+func (c *Client) MapGet(hexLocation *hexcli.HexLocation, radius int64) (hexList *hexcli.HexLocationList, err error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
 	request := hexcli.HexagonGetRequest{
-		Hex: &hexcli.Hex{
-			X: hex.X,
-			Y: hex.Y,
-			Z: hex.Z,
+		HexLoc: &hexcli.HexLocation{
+			X: hexLocation.X,
+			Y: hexLocation.Y,
+			Z: hexLocation.Z,
 		},
 		Radius: radius,
 		Fill:   false,
--- internal/pkg/hexclient/repo.go
+++ internal/pkg/hexclient/repo.go
@@ -1,9 +1,14 @@
 package hexclient
 
 import (
+	"bufio"
+	"encoding/csv"
 	"fmt"
 	"github.com/3vilm33pl3/hexcli/internal/pkg/hexcli"
 	"github.com/spf13/cobra"
+	"log"
+	"os"
+	"strconv"
 )
 
 var repoCmd = &cobra.Command{
@@ -16,21 +21,66 @@ var repoCmd = &cobra.Command{
 }
 
 var repoAddCmd = &cobra.Command{
-	Use:   "add [ref]",
-	Short: "add hexagon to repository with reference [ref]",
+	Use:   "add [ref] [exits]",
+	Short: "add hexagon to repository with reference [ref] and [exits]",
 	Run: func(cmd *cobra.Command, args []string) {
 		serverAddr, _ := cmd.Flags().GetString("addr")
 		secure, _ := cmd.Flags().GetBool("secure")
 
 		client, err := NewClient(serverAddr, secure)
-		var refList hexcli.HexRefList
+		var infoList hexcli.HexInfoList
 
-		for _, ref := range args {
-			refList.Ref = append(refList.Ref, &hexcli.HexReference{Ref: ref})
+		ref := args[0]
+		exits, err := strconv.Atoi(args[1])
+		if err != nil {
+			fmt.Printf("Error parsing exits %s", err)
+		}
+		infoList.HexInfo = append(infoList.HexInfo, &hexcli.HexInfo{ID: ref, Exits: uint32(exits)})
+
+		err = client.RepoAddHexagonInfo(&infoList)
+		if err != nil {
+			fmt.Printf("Error connecting %s", err)
+			return
 		}
 
-		err = client.RepoAddHexagon(&refList)
+	},
+}
 
+var repoAddFileCmd = &cobra.Command{
+	Use:   "file [filename]",
+	Short: "add hexagon from file [filename] to repository",
+	Run: func(cmd *cobra.Command, args []string) {
+		serverAddr, _ := cmd.Flags().GetString("addr")
+		secure, _ := cmd.Flags().GetBool("secure")
+
+		client, err := NewClient(serverAddr, secure)
+		var infoList hexcli.HexInfoList
+
+		f, err := os.Open(args[0])
+		if err != nil {
+			fmt.Printf("Error opening file %s", args[0])
+			return
+		}
+		rc := bufio.NewReader(f)
+		csvLines, err := csv.NewReader(rc).ReadAll()
+		if err != nil {
+			log.Printf("Error reading hexdata file: %v", err)
+			return
+		}
+
+		for i, line := range csvLines {
+			exits, err := strconv.Atoi(line[1])
+			if err != nil {
+				fmt.Printf("Skipping line %d: %s\n", i, err)
+			}
+			hexInfo := &hexcli.HexInfo{
+				ID:    line[0],
+				Exits: uint32(exits),
+			}
+			infoList.HexInfo = append(infoList.HexInfo, hexInfo)
+		}
+
+		err = client.RepoAddHexagonInfo(&infoList)
 		if err != nil {
 			fmt.Printf("Error connecting %s", err)
 			return
@@ -47,17 +97,66 @@ var repoDelCmd = &cobra.Command{
 		secure, _ := cmd.Flags().GetBool("secure")
 
 		client, err := NewClient(serverAddr, secure)
-		var refList hexcli.HexRefList
+		var infoList hexcli.HexIDList
 
-		for _, ref := range args {
-			refList.Ref = append(refList.Ref, &hexcli.HexReference{Ref: ref})
+		for _, id := range args {
+			infoList.HexID = append(infoList.HexID, id)
 		}
 
-		err = client.RepoDeleteHexagon(&refList)
+		err = client.RepoDeleteHexagonInfo(&infoList)
 
 		if err != nil {
 			fmt.Printf("Error connecting %s", err)
 			return
+		}
+	},
+}
+
+var repoGetCmd = &cobra.Command{
+	Use:   "get [ref]",
+	Short: "get hexagon info from repository with reference [ref] ",
+	Run: func(cmd *cobra.Command, args []string) {
+		serverAddr, _ := cmd.Flags().GetString("addr")
+		secure, _ := cmd.Flags().GetBool("secure")
+
+		client, err := NewClient(serverAddr, secure)
+		var infoList hexcli.HexIDList
+
+		for _, id := range args {
+			infoList.HexID = append(infoList.HexID, id)
+		}
+
+		result, err := client.RepoGetHexagonInfo(&infoList)
+
+		if err != nil {
+			fmt.Printf("Error connecting %s", err)
+			return
+		}
+
+		for _, hex := range result.HexInfo {
+			fmt.Printf("%s %d \n", hex.ID, hex.Exits)
+		}
+	},
+}
+
+var repoGetAllCmd = &cobra.Command{
+	Use:   "all",
+	Short: "get all hexagon info from repository",
+	Run: func(cmd *cobra.Command, args []string) {
+		serverAddr, _ := cmd.Flags().GetString("addr")
+		secure, _ := cmd.Flags().GetBool("secure")
+
+		client, err := NewClient(serverAddr, secure)
+
+		result, err := client.RepoGetAllHexagonInfo()
+
+		if err != nil {
+			fmt.Printf("Error connecting %s", err)
+			return
+		}
+
+		for _, hex := range result.HexInfo {
+			fmt.Printf("%s %d \n", hex.ID, hex.Exits)
 		}
 	},
 }
